7d0fe59bc2c2dd5d1f0da1b4cc00ed63cdfb01d2
dignifiedquire <me@dignifiedquire.com>
2026-02-06 23:37:55 +0100

    feat(rust): port capture levels adjuster and RMS level

--- crates/webrtc-apm/src/lib.rs
+++ crates/webrtc-apm/src/lib.rs
@@ -5,9 +5,11 @@
 
 pub(crate) mod audio_buffer;
 pub(crate) mod audio_samples_scaler;
+pub(crate) mod capture_levels_adjuster;
 pub(crate) mod echo_detector;
 pub(crate) mod high_pass_filter;
 pub(crate) mod residual_echo_detector;
+pub(crate) mod rms_level;
 pub(crate) mod splitting_filter;
 pub(crate) mod stream_config;
 pub(crate) mod three_band_filter_bank;
